ef1698d70e2935c95c66e7d3b7e11ec92ca2d079
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-14 15:49:00 -0300

    :lock: Requisitando o mongoose e fazendo a conexão ao MongoDB

--- backend/Server.js
+++ backend/Server.js
@@ -1,13 +1,27 @@
-const Express = require ("express")
+const express = require ("express")
 const cors = require("cors")
-const App = Express()
+const mongoose = require("mongoose")
+const App = express()
 App.use(cors())
 
+//Forma de ler json
+App.use(express.urlencoded({
+    extends:true,
+}))
+App.use(express.json())
+//teste do endpoint
 App.get('/', (req,res)=>{
-    res.send("Sua requisição funcionou!")
+    res.json({message: "Olá!"})
 })
-
-
-App.listen(3001,()=>{
-    console.log('Servidor está rodando')
+//Conexão ao banco de dados
+mongoose.connect(
+    "mongodb+srv://lucasgnam12:2uYl7QsmNjNL94zp@apliclusteer.xhwplkk.mongodb.net/bandodaapi?retryWrites=true&w=majority"
+)
+.then(()=>{
+    console.log('Conectamos ao MongoDB!')
+    App.listen(3001,()=>{
+        console.log('Servidor está rodando')
+    })
 })
+.catch((err)=>console.log(err))
+//
